031132b0365e4ee6ce1954ad4b06ee0ebe8b1d62
i-kumagai <i-kumagai@bit-isle.co.jp>
2014-12-24 15:41:19 +0900

    modify usage to testdata script

--- gatling/04_setup-testdata.sh
+++ gatling/04_setup-testdata.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+echo 
+echo "Usage : sh $0 <datacount>"
+echo "This script insert data of the specified number and web response time 3 times."
+echo "Notice: This script truncate current data and insert new data. "
 
-# MY_DB=192.168.0.1
-# MY_LBS=192.168.0.7
-echo "usage : sh $0 <datacount>"
+echo "  <datacount>           : The number of data to insert."
+echo "  env[MY_DB]  (required): DB Server address or hostname.    Use this to execute mysql command via ssh."
+echo "  env[MY_LBS] (required): Load Balancer address or hostname.Use this to check resoponse time by curl command on 80 port"
+
+echo "-- Check your patameter. --"
+echo "[MY_DB]     = ${MY_DB}"
+echo "[MY_LBS]    = ${MY_LBS}"
+echo "<datacount> = ${1}"
+
+echo "Press any key to continue or Ctrl-C to quit. :"
+read 
 
 echo "truncate contents;" |ssh ${MY_DB} mysql -u root sample_bbs
 for i in `seq 1 ${1}`
@@ -10,9 +22,10 @@ do
   echo "insert into contents values(null,now(),${i},null);"|ssh ${MY_DB} mysql -u root sample_bbs
 done
 
-echo "select count(*) from contents;"|ssh ${MY_DB} mysql -u root sample_bbs
+echo -n "Data Count: "
+echo "select count(*) as datacount from contents;"|ssh ${MY_DB} mysql -u root sample_bbs --skip-column-names
 
-curl -L ${MY_LBS} -o /dev/null -w "response time: %{time_total}\n" 2> /dev/null
-curl -L ${MY_LBS} -o /dev/null -w "response time: %{time_total}\n" 2> /dev/null
-curl -L ${MY_LBS} -o /dev/null -w "response time: %{time_total}\n" 2> /dev/null
+curl -L ${MY_LBS} -o /dev/null -w "response time(1): %{time_total}\n" 2> /dev/null
+curl -L ${MY_LBS} -o /dev/null -w "response time(2): %{time_total}\n" 2> /dev/null
+curl -L ${MY_LBS} -o /dev/null -w "response time(3): %{time_total}\n" 2> /dev/null
 
